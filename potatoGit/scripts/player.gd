e56330b2f6d33444b09133f42067deed4801dee0
unknown <MediaLab@DELLTTAA.cern.ch>
2025-06-24 16:46:57 +0200

    Added test comment

--- potatoGit/scripts/player.gd
+++ potatoGit/scripts/player.gd
@@ -10,6 +10,7 @@ func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+		#test comment
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
