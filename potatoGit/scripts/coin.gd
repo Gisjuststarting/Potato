d6cf4c053f0e79eb996193269e46872fa84e1a0a
Gui <MediaLab@DELLTTAA.cern.ch>
2025-06-25 16:48:32 +0200

    Enemys and gamemanager added. Rest was just upgrages

--- potatoGit/scripts/coin.gd
+++ potatoGit/scripts/coin.gd
@@ -1,6 +1,8 @@
 extends Area2D
 
+@onready var game_manager = %"Game manager"
 
 func _on_body_entered(_body: Node2D) -> void:
-	print("+1 Coin!")
+	game_manager.add_point()
 	queue_free()
+	
--- potatoGit/scripts/player.gd
+++ potatoGit/scripts/player.gd
@@ -3,25 +3,61 @@ extends CharacterBody2D
 
 const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
+@onready var animated_sprite = $AnimatedSprite2D
+var current_angle:float = 0.0
+var is_jump:bool = false
+var rot_direction:float = 1.0
 
-func _physics_process(delta: float) -> void:
-	#print("potato")
-	
+func _physics_process(delta: float) -> void:	
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
-		#test comment
-		#another comment
-		#Potato
+		velocity += get_gravity() * delta 		
+		if is_jump:
+			current_angle += delta*720*rot_direction
+		rotation_degrees = current_angle
+		if current_angle >360.0:
+			rotation_degrees = 0.0
+			
+	else:
+		current_angle = 0.0
+		rotation_degrees = 0.0
+		is_jump = false
+
+
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY 
+		is_jump = true
+		#current_angle = 0
+	
+
+
+
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("move_left", "move_right")
+	if direction != 0:
+		rot_direction = direction
+	#Flip the sprite
+	if direction > 0:
+		animated_sprite.flip_h = false
+	elif direction < 0:
+		animated_sprite.flip_h = true
+	
+	# Play animations
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite.play("Idle")
+		else:
+			animated_sprite.play("Run")
+	else:
+		animated_sprite.play("Jump")
+	
+		
+	
 	
 	if direction:
 		velocity.x = direction * SPEED
@@ -30,3 +66,4 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)		
 
 	move_and_slide()
+	
